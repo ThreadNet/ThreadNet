0ed6cfed69cf146ebc4694aa4f3785e7709ff315
Brian Pentland <pentlan2@msu.edu>
2017-11-06 13:34:58 -0500

    Added a comment

--- R/Global.R
+++ R/Global.R
@@ -1,9 +1,9 @@
 ##########################################################################################################
 # THREADNET:  SHINY GLOBAL
 
-# (c) 2017 Michigan State University. This software may be used according to the terms provided in the 
+# (c) 2017 Michigan State University. This software may be used according to the terms provided in the
 # GNU General Public License (GPL-3.0) https://opensource.org/licenses/GPL-3.0?
-# Absolutely no warranty! 
+# Absolutely no warranty!
 ##########################################################################################################
 
 # 15 October Point of view code has solidified somewhat
@@ -15,14 +15,15 @@ library(ngram)
 library(stringr)
 library(stringdist)
 library(ggplot2)
-library(networkD3)  
+library(networkD3)
 library(visNetwork)
 library(xesreadR)
 library(colorspace)
 library(igraph)
 library(TraMineR)
 
-        
+# this is a change
+
 source("ThreadNet_Core.R")
 source("ThreadNet_Misc.R")
 source("ThreadNet_Graphics.R")
