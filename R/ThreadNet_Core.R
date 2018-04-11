bfe3a60dc69854532ccbcc35dfa7a4135354bd34
Brian Pentland <pentlan2@msu.edu>
2018-04-11 07:32:20 -0400

    Added function to compute generativity (in-degree and out-degree) and to prevent error in count_ngrams where n > nwords in text_vector

--- R/ThreadNet_Core.R
+++ R/ThreadNet_Core.R
@@ -103,7 +103,9 @@ count_ngrams <- function(o,TN,CF,n){
 
 
   # Cannot put all the values in one long string.  Need a vector of strings, one for each thread, delimited by a space
-  text_vector = thread_text_vector(o,TN,CF,' ')
+  # the function long_enough filters out the threads that are shorter than n
+  # use comma for the delimiter here
+  text_vector = long_enough( thread_text_vector(o,TN,CF,' '), n, ',')
 
    # print("text_vector")
    # print(text_vector)
@@ -112,6 +114,7 @@ count_ngrams <- function(o,TN,CF,n){
   #    col 1 = ngrams
   #    col 2 = freq
   #    col 3 = prop (proportion that matches)
+
   ng = get.phrasetable(ngram(text_vector,n))
 
   # add a column here for the length of the ngram -- useful later!
@@ -837,7 +840,8 @@ selectize_frequent_ngrams<- function(e, TN, CF, minN, maxN, threshold){
 
 
 # combined set of frequent ngrams
-frequent_ngrams <- function(e, TN, CF, minN, maxN, threshold){
+# add parameter to make maximal a choice
+frequent_ngrams <- function(e, TN, CF, minN, maxN, threshold, onlyMaximal=FALSE){
 
   # initialize the output
   ng = count_ngrams(e,TN, CF,minN)
@@ -850,8 +854,8 @@ frequent_ngrams <- function(e, TN, CF, minN, maxN, threshold){
   # remove the rows that happen once and only keep the columns we want
   ng=ng[ng$freq>threshold,c('ngrams','freq', 'len')]
 
-  # just take the maximal ones
-   ng=maximal_ngrams(ng)
+  # just take the maximal ones if so desired
+  if (onlyMaximal) { ng=maximal_ngrams(ng)  }
 
   # return the set sorted by most frequent
   return(ng[order(-ng$freq),])
@@ -893,12 +897,80 @@ support_level <- function(tv, ng) {
   ng$support = unlist(lapply(1:nrow(ng), function(i){
     length(grep(ng$ngrams[i],tv)) })
         )/totalN
+
+  # toss in the generativity level
+  ng = generativity_level(tv,ng)
+
   return(ng)
 }
 
+# compute the generativity = in-degree and out-degree
 generativity_level<- function(tv, ng){
 
+  # for each ngram, look at the next longer size
+  # Find the n+1-grams that match (as in the code for maximal ngrams).
+  # There are two possibilities -- matching in the first or second position
+  # The number of matches in the first position =  the out-degree
+  # The number of matches in the second position =  the in-degree
+  # if so desired, it should be possible to keep a list.
+
+  # problem is that the tokens can be 1-3 characters long, and there are spaces...
+
+  # Big Idea for frequent n-grams: use the DT:: and let people sort, select and apply all the ngrams they want.
+  # Name them using the tokens but with a different delimiter to avoid confusion.  Go Crazy!
+
+  # convert to spaces
+  tv=str_replace_all(tv, ',',' ')
+
+  # first get the range we are looking for
+  nList = unique(ng$len)
+
+  z=list()
+
+  # loop through them
+  for (n in nList){
+
+    # print(paste('n = ',n))
+    #pick the ngrams of length n from the list given
+    ngn= ng[ng$len==n,]
 
 
+    # get ngrams of length n+1 -- make sure the threads are long enough
+    ngplus = get.phrasetable(ngram( long_enough(tv,n+1, ' '), n+1))
+
+    # this picks out the ones that match
+  w = lapply(1:nrow(ngn), function(i){
+    grep(ngn$ngrams[i],ngplus$ngrams)} )
+
+  #print(w)
+  # print('z = ')
+  zplus = lapply(1:nrow(ngn), function(i){
+    str_locate(ngplus$ngrams[w[[i]]],ngn$ngrams[i])  } )
+
+  # print(z)
+
+  z = c(z,zplus)
+
+  }
+
+  # compute the in and out degree
+  ng$out_degree = unlist( lapply(1:nrow(ng), function(i){
+    zm=z[[i]]
+    length( zm[zm[,1]==1,1] )  } ))
+
+  ng$in_degree = unlist(lapply(1:nrow(ng), function(i){
+    zm=z[[i]]
+    length( zm[zm[,1]>1,1] )  } ))
+
+  ng$generativity = lapply(1:nrow(ng), function(i) {ng$out_degree[i] * ng$in_degree[i]})
+
+  return(ng)
 }
 
+# to avoid errors in count_ngrams, make sure the length of each thread in the text_vector tv is longer than the n-gram size, n
+# this gets used in various places so need to pass in the delimiter
+long_enough = function(tv,n,delimiter){
+
+return(tv[ unlist(lapply(1:length(tv), function(i) {length(unlist(strsplit(tv[[i]],delimiter)))>=n})) ])
+
+}
