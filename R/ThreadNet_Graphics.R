61b91a3d843093cfd6993ad6846185002d6a7d2c
patte393 <patte393@msu.edu>
2018-04-24 13:09:59 +0000

    networks nodes back to label

--- R/ThreadNet_Graphics.R
+++ R/ThreadNet_Graphics.R
@@ -454,7 +454,7 @@ forceNetworkD3 <- function(et,TN, grp, zoom_level){
   # print(edges)
 
   return( forceNetwork(Links = edges, Nodes = nodes, Source = "from",
-                       Target = "to", Value = "Value", NodeID = "Group",
+                       Target = "to", Value = "Value", NodeID = "label",
                        Group = "Group", opacity = 1, zoom = T, bounded = FALSE, clickAction = 'Shiny.onInputChange("Group", d.name)'))
 }
 
