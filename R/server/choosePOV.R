d33d8fb66ea9265bf0cc8964f193c8a928140535
Brian Pentland <pentlan2@msu.edu>
2018-05-29 23:28:27 -0400

    Grabbed Ezra's changes on the POV tab

--- R/server/choosePOV.R
+++ R/server/choosePOV.R
@@ -1,57 +1,64 @@
-# Server Output Functions for Choose POV Tab 
 
 #### Define Threads sub-tab ####
 
-output$POV_Tab_Controls_2 <- renderUI({
-	checkboxGroupInput(
-		"THREAD_CF_ID",
-		"Select columns to define threads:",
-		cfnames(selectOccFilter()),
-		selected = get_THREAD_CF(),
-		inline = TRUE
-	)
+output$povThreadSelector <- renderUI({
+  checkboxGroupInput(
+    "THREAD_CF_ID",
+    "Select columns to define threads:",
+    get_CF(),
+    selected = "",
+    inline = TRUE
+  )
 })
 
-output$ContextFlowers_2 <- renderPlotly({
-	CF_multi_pie(
-		selectOccFilter(),
-		get_THREAD_CF()
-	)
+output$ContextFlowers_Threads <- renderPlotly({
+  CF_multi_pie(
+    selectOccFilter(),
+    get_THREAD_CF()
+  )
 })
 
 #### Define Events sub-tab ####
 
-output$POV_Tab_Controls_3 <- renderUI({
-	checkboxGroupInput(
-		"EVENT_CF_ID",
-		"Select columns to mark events:",
-		cfnames(selectOccFilter()),
-		selected = get_EVENT_CF(),
-		inline = TRUE
-	)
+output$povEventSelector <- renderUI({
+  checkboxGroupInput(
+    "EVENT_CF_ID",
+    "Select columns to mark events:",
+    get_CF(),
+    selected = "",
+    inline = TRUE
+  )
 })
 
-output$ContextFlowers_3 <- renderPlotly({
-	CF_multi_pie(
-		selectOccFilter(),
-		get_EVENT_CF()
-	)
+output$ContextFlowers_Events <- renderPlotly({
+  CF_multi_pie(
+    selectOccFilter(),
+    get_EVENT_CF()
+  )
 })
-
-#### Preview Threads sub-tab ####
-
-output$previewThreadMap_1 <- renderPlotly({
-	threadMap(
-		threadedOcc(),
-		"threadNum",
-		"tStamp",
-		newColName(get_EVENT_CF()),
-		16
-	)
-})
-
-output$Preview_Thread_Output_1 <- renderText({ paste(numThreads(threadedOcc(), "threadNum"),"threads in the selected data.")})
 
 #### Preview Data sub-tab ####
 
-output$Thread_Tab_Output_1 <- DT::renderDataTable({ threadedOcc()})
+########
+# The POV tabs reconstruct the data into threads by sorting by tStamp and
+# adding columns for threadNum and seqNum for the selected POV in ThreadOccByPOV
+output$povDataThreads <- DT::renderDataTable({
+
+  # thread occurences by POV
+  threadedOcc()
+})
+
+#### Add new Dataset sub-tab ####
+
+output$addPOV <- renderUI({
+  tags$div(
+    align="left",
+    textInput(
+      "POVMapName",
+      label = h4(paste("Enter label for this POV mapping")),
+      value = ""
+    ),
+    actionButton("addPOVButton", "Save mapping")
+  )
+})
+
--- R/ui/choosePOV.R
+++ R/ui/choosePOV.R
@@ -1,33 +1,45 @@
-tabPanel(value = "choosePOV",
-    "Choose POV",
-    helpText('Select columns from your data to define your point of view. You MUST click on either Preview Threads or Preview Data before proceeding.'),
-    tags$hr(),
-    tabsetPanel(
-        id = "tabs",
-        tabPanel(value = "defineThreads",
-            "Define Threads",
-            tags$h4("Threads are defined by contextual features that STAY THE SAME during a thread. At least ONE is required."),
-            uiOutput("POV_Tab_Controls_2"),
-            plotlyOutput("ContextFlowers_2")
-        ),
-        tabPanel(value = "defineEvents",
-            "Define Events",
-            tags$h4("Events are marked by contextual features that CHANGE within the threads. At least ONE is required."),
-            uiOutput("POV_Tab_Controls_3"),
-            plotlyOutput("ContextFlowers_3")
-        ),
-        # tabPanel(
-        #     "Preview Threads",
-        #     tags$h4("Threads based on selected POV"),
-        #     verbatimTextOutput("Preview_Thread_Output_1" ),
-        #     # ** add conditional panels here to choose output **
-        #     plotlyOutput("previewThreadMap_1")
-        # ),
-        tabPanel(value = "showThreads",
-            "*** CLICK HERE TO PROCEED ***",
-            tags$h4("This table shows the data threaded from your chosen POV"),
-            DT::dataTableOutput("Thread_Tab_Output_1")
-        )
-    )
-)
+# UI: Choose POV tab definitions
 
+tabPanel(
+
+  value = "choosePOV",
+
+  "Choose POV",
+
+  tags$hr(),
+
+  tabsetPanel(
+
+    id = "tabs",
+
+    tabPanel(
+      value = "defineThreads",
+      "Define Threads",
+      tags$h4("Threads are defined by contextual features that STAY THE SAME during a thread. At least ONE is required."),
+      uiOutput("povThreadSelector"),
+      plotlyOutput("ContextFlowers_Threads")
+    ),
+
+    tabPanel(
+      value = "defineEvents",
+      "Define Events",
+      tags$h4("Events are marked by contextual features that CHANGE within the threads. At least ONE is required."),
+      uiOutput("povEventSelector"),
+      plotlyOutput("ContextFlowers_Events")
+    ),
+
+    tabPanel(
+      value = "showThreads",
+      "Review Data",
+      tags$h4("This table shows the data threaded from your chosen POV."),
+      DT::dataTableOutput("povDataThreads")
+    ),
+
+    tabPanel(
+      value = "saveThreads",
+      "Save Data",
+      tags$h4("Save this dataset to continue."),
+      uiOutput("addPOV")
+    )
+  )
+)
