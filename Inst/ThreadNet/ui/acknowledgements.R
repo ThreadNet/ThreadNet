a2a300827161fa7a73dca4562dd36521e6fad16a
Brian Pentland <pentlan2@msu.edu>
2018-06-07 17:16:59 +0000

    Updated acknowledgements

--- Inst/ThreadNet/ui/acknowledgements.R
+++ Inst/ThreadNet/ui/acknowledgements.R
@@ -1,14 +1,27 @@
 tabPanel(
     "Acknowledgements",
-    tags$h4("Support"),
-    tags$a(href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1734237","NSF SES-1734237"),
+    tags$h4("Author"),
+    tags$p("Brian Pentland, Michigan State University"),
+
+    tags$h4("Contact"),
+    tags$mailto("ThreadNetHelp@gmail.com"),
+
+    tags$h4("Financial Support"),
+    tags$a(href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1734237","NSF SES-1734237",target="_blank"),
     tags$p("Antecedents of Complexity in Healthcare Routines"),
+
     tags$h4("Code Gurus"),
-    tags$p("Yu Lucy Han, Ezra Brooks, Patrick Bills, Danielle Barnes, Morgan Patterson, Douglas Krum"),
+    tags$p("Danielle Barnes, Patrick Bills, Ezra Brooks, Yu Lucy Han, Morgan Patterson, Douglas Krum, Mahmood Shafeie Zargar"),
+
     tags$h4("Collaborators"),
     tags$p("Katharina Dittrich, Martha Feldman, Ken Frank, Thorvald Haerem, Inkyu Kim, Waldemar Kremser, Christian Mahringer, Alice Pentland, Jan Recker, Sudhanshu Srivastava, Julie Ryan Wolf, George Wyner "),
-    tags$h4("Related Publications"),
-    tags$a(href="http://routines.broad.msu.edu/resources/","http://routines.broad.msu.edu/resources/" ),
+
+    tags$h4("Documentation and Related Publications"),
+    tags$a(href="http://routines.broad.msu.edu/resources/","http://routines.broad.msu.edu/resources/",target="_blank" ),
+
     tags$h4("ThreadNet 2 (MatLab version)"),
-    tags$a(href="http://routines.broad.msu.edu/ThreadNet/","http://routines.broad.msu.edu/ThreadNet/" )
+    tags$a(href="http://routines.broad.msu.edu/ThreadNet/","http://routines.broad.msu.edu/ThreadNet/",target="_blank" ),
+
+    tags$h4("Official Song"),
+    tags$a(href="Doctor Decade: Tell Us a Story","https://www.youtube.com/watch?v=tSnoHWBYA8U",target="_blank" )
 )
